7dc307384fe8301b1f173ec969f968f206035551
Andrei Lucaci <contact@andreilucaci.ro>
2018-03-09 19:08:25 +0200

    Added tests

---
+++ AwesomeBot.Tests/BotTests.cs
@@ -0,0 +1,31 @@
+﻿using AwesomeBot.Bot;
+using AwesomeBot.Field;
+using NUnit.Framework;
+
+namespace AwesomeBot.Tests
+{
+    [TestFixture]
+    public class BotTests
+    {
+        public const string End =
+                ".,.,.,.,.,.,.,0,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,x,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,x,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,x,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,x,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,x,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,x,.,x,x,x,x,x,x,x,.,.,.,.,.,.,x,x,.,x,.,x,x,x,x,x,.,x,x,x,x,x,x,x,.,.,.,x,x,x,x,x,.,x,x,x,x,x,x,x,.,.,.,x,x,x,x,x,.,x,x,x,x,x,x,.,.,.,1,x,x,x,x,x,.,x,x,x,x,x,x,.,.,.,x,x,x,x,x,x,.,x,x,x,x,x,x,.,.,.,x,x,x,x,x,x,.,x,x,x,x,x,x,.,.,.,x,x,x,x,x,x,.,x,x,x,x,x,x,.,.,.,x,x,x,x,x,x,.,x,x,x,x,x,x,.,.,.,x,x,x,x,x,x"
+            ;
+
+        [Test]
+        public void Test_Bot_Test()
+        {
+            var board = new Board
+            {
+                Width = 16,
+                Height = 16
+            };
+
+            board.InitField();
+            board.ParseFromString(End);
+
+            var bot = new Bot.AwesomeBot();
+
+            var move = bot.MoveBot(new BotState {Board = board});
+        }
+    }
+}
